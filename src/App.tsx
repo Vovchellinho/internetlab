5928a8bb072c6b26a846e82914e1cffe379b83b9
vovhcellinho <ot4ertsov@gmail.com>
2024-06-28 20:43:55 +0300

    накидал структуру проекта

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "@components/Header";
+
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<h1>asdasdasdasdasd scasc</h1>
+			<span>sfaf fsa dsad asd s</span>
+		</div>
+	);
+}
+
+export default App;

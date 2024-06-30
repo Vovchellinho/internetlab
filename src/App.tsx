848361a621331bae2b2ca001a96e932a75ad5cf7
vovhcellinho <ot4ertsov@gmail.com>
2024-06-30 09:26:24 +0300

    сделал footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HowItWorks from "@components/HowItWorks";
 import Review from "@components/Review";
 import MoreInfo from "@components/MoreInfo";
 import Form from "@components/Form";
+import Footer from "@components/Footer";
 
 const App = () => {
 	return (
@@ -16,6 +17,7 @@ const App = () => {
 				<MoreInfo />
 				<Form />
 			</main>
+			<Footer/>
 		</>
 	);
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./style.module.css";
+
+const Footer = () => {
+	return (
+		<footer className={styles.container}>
+			<span>© 2021 Лаборатория интернет</span>
+		</footer>
+	);
+};
+
+export default Footer;
